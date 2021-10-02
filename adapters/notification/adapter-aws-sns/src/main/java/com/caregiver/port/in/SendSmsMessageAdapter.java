3153386a79bd941191350a11623fe38b21edc86c
truman-show <ljh86091@gmail.com>
2021-10-03 04:26:32 +0900

    AWS SMS 실패시 예외를 응답하도록 수정하라
    - 예외 발생시 로그 메세지를 남기도록 추가했습니다.
    - SMS 실패시 예외를 응답하도록 수정했습니다.

--- adapters/notification/adapter-aws-sns/src/main/java/com/caregiver/port/in/SendSmsMessageAdapter.java
+++ adapters/notification/adapter-aws-sns/src/main/java/com/caregiver/port/in/SendSmsMessageAdapter.java
@@ -1,14 +1,19 @@
 package com.caregiver.port.in;
 
+import static com.caregiver.common.exception.ErrorCode.INTERNAL_SERVER_ERROR;
+
 import com.amazonaws.services.sns.model.PublishRequest;
 import com.caregiver.common.annotation.NotificationAdapter;
+import com.caregiver.common.exception.BusinessException;
 import com.caregiver.config.AwsSnsClient;
 import com.caregiver.user.port.out.NotificationSmsPort;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * SMS 문자 메세지를 보냅니다.
  */
+@Slf4j
 @NotificationAdapter
 @RequiredArgsConstructor
 public class SendSmsMessageAdapter implements NotificationSmsPort {
@@ -18,15 +23,25 @@ public class SendSmsMessageAdapter implements NotificationSmsPort {
   /**
    * SMS 문자메세지를 보냅니다.
    *
-   * @param notificationSmsCommand SMS 요청.
+   * @param command SMS 요청.
    */
   @Override
-  public void sendSmsMessage(NotificationSmsCommand notificationSmsCommand) {
-    awsSnsClient.snsClient()
-        .publish(
-            new PublishRequest().withMessage(notificationSmsCommand.getMessage())
-                .withPhoneNumber(notificationSmsCommand.concatCountryCodeMobile())
-                .withMessageAttributes(awsSnsClient.getMessageAttributeValue())
-        );
+  public void sendSmsMessage(NotificationSmsCommand command) {
+    try {
+
+      awsSnsClient.snsClient()
+          .publish(
+              new PublishRequest()
+                  .withMessage(command.getMessage())
+                  .withPhoneNumber(command.concatCountryCodeMobile())
+                  .withMessageAttributes(awsSnsClient.getMessageAttributeValue())
+          );
+
+    } catch (RuntimeException e) {
+      log.error("AWS SMS publish 예외 mobile : {}, exception : {}",
+          command.getMobile(), e.getCause());
+      throw new BusinessException(INTERNAL_SERVER_ERROR, "SMS 문자 발송에 실패 하였습니다.");
+    }
   }
+
 }
